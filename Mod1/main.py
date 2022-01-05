f73d043a83a710b3fb7c9298d3d3b400e6f5852c
Allison Valdez <ajsnipes@usfca.edu>
2022-01-05 14:27:03 -0800

    working on notes

--- Mod1/main.py
+++ Mod1/main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+"""
+time complexity: O(n) time
+space complexity: O(1) because we are not storing any values other than the
+last two numbers calculated
+"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_nth_fib(num):
+    """
+    This is the iterative version of the algorithm. Our solution relies on
+    only saving the last two calculations of the fibonacci sequence for quick
+    calculation of the sequence.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    :param num: the number we wish to calculate the fibonacci sequence for
+    :return: returns calculated fibonacci value
+    """
+
+    # the base case of the algorithm
+    last_two_values = [0, 1]
+    # counter for the algorithm starting at 3 (since base cases controlled for)
+    counter = 3
+
+    # while loop that iterates for the fibonacci sequence
+    while counter <= num:
+        # next_fib_sequence is equal to the summation of base cases
+        next_fib_sequence = last_two_values[0] + last_two_values[1]
+
+        # next_fib_sequence[0] takes on the value of [1]
+        last_two_values[0] = last_two_values[1]
+
+        # next_fib_sequence[1] takes on the value of [n]
+        last_two_values[1] = next_fib_sequence
+
+        # increase the counter for the while loop to continue
+        counter += 1
+
+    # control for the base case of being at [1}
+    return last_two_values[1] if num > 1 else last_two_values[0]
+
+
+print(get_nth_fib(5))
